00f1f93b4907f0f02e7d2cb9751e3ab7df090d5b
Sanchez <83212752+AlexYez@users.noreply.github.com>
2025-02-23 13:04:44 +0300

    v 1.0.2

--- __init__.py
+++ __init__.py
@@ -3,8 +3,11 @@ from .nodes import *
 
 #  Map all your custom nodes classes with the names that will be displayed in the UI.
 NODE_CLASS_MAPPINGS = {
-    "Timesaver Youtube Chapters": EDLToYouTubeChapters,
-    "Timesaver Files Downloader": DownloadFilesNode,
+    "TS Youtube Chapters": EDLToYouTubeChapters,
+    "TS Files Downloader": DownloadFilesNode,
+    "DownloadFilesNode": DownloadFilesNode,
+    "TS Equirectangular to Cube": EquirectangularToCubemapFaces,
+    "TS Cube to Equirectangular": CubemapFacesToEquirectangular,
 }
 
 
